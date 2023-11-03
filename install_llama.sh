8a2821cb4e8e8a4710706c7c90d32d5051e58c50
tf-josh <josh.jaroudy@taylorfry.com.au>
2023-11-03 23:16:33 +1100

    download llama 7b

--- install_llama.sh
+++ install_llama.sh
@@ -8,3 +8,8 @@ sourec venv_llama/bin/activate
 # install llama
 pip install llama-cpp-python
 
+# download the model
+# use hugging face hub to download
+pip3 install huggingface-hub>=0.17.1
+
+huggingface-cli download TheBloke/Llama-2-7B-GGUF llama-2-7b.Q6_K.gguf --local-dir ~/projects/llms/llama/ --local-dir-use-symlinks False
